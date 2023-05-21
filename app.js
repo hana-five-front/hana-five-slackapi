bf24167d229d20e930dde668eae41dcb490019a1
eternalclash <suchasucha@naver.com>
2023-05-21 15:43:54 +0900

    Feat: POST요청 완료

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const { WebClient } = require('@slack/web-api');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { WebClient } = require("@slack/web-api");
 
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-// health check
-app.get('/ping', (req, res) => {
-  res.status(200).json({ message: 'pong' });
+// WebClient 인스턴스 생성
+const slackClient = new WebClient(process.env.SLACK_TOKEN);
+
+// Slack에 메시지 보내는 함수
+async function sendMessageToSlack(text) {
+  try {
+    const result = await slackClient.chat.postMessage({
+      channel: process.env.CHANNEL,
+      text: text,
+    });
+
+    console.log("Message sent:", result);
+  } catch (error) {
+    console.error("Error sending message:", error);
+  }
+}
+
+app.post("/slackapi", (req, res) => {
+  const text = req.body.text;
+  sendMessageToSlack(text);
 });
 
-app.get('/slackapi', (req, res) => {
+app.get("/slackapi", (req, res) => {
   const slackToken = process.env.SLACK_TOKEN;
   const client = new WebClient(slackToken);
   // 대화 내역을 가져오기 위해 API 요청을 만듭니다.
@@ -41,7 +58,7 @@ app.get('/slackapi', (req, res) => {
         // 작성 날짜 표시
         const date = new Date(message.ts * 1000);
         // 줄바꿈 문자를 기준으로 첫 번째 줄과 나머지 줄 분리
-        const lines = message.text.split('\n');
+        const lines = message.text.split("\n");
         const title = lines[0];
         const contents = lines.slice(1);
         data.push({ userName, date, title, contents });
