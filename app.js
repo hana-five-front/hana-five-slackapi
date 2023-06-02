e17d5ad376c94c7dce16592af942ebe26cca8810
eternalclash <suchasucha@naver.com>
2023-06-02 21:18:36 +0900

    수정

--- app.js
+++ app.js
@@ -109,6 +109,7 @@ async function connectToRedis() {
     }
   }, 1800000);
 
+
   async function addArrayOfObjectsToRedis(key, arrayOfObjects) {
     const serializedArray = JSON.stringify(arrayOfObjects);
     await client.set(key, serializedArray, (error, result) => {
@@ -135,6 +136,7 @@ connectToRedis()
 
 // WebClient 인스턴스 생성
 const slackClient = new WebClient(process.env.SLACK_NOTICE_TOKEN);
+
 // Slack에 메시지 보내는 함수
 async function sendMessageToSlack(text) {
   try {
@@ -148,11 +150,11 @@ async function sendMessageToSlack(text) {
     console.error('Error sending message:', error);
   }
 }
-
+const qnaClient = new WebClient(process.env.SLACK_QNA_TOKEN)
 // SlackQNA에 메시지 보내는 함수
 async function sendQnaMessageToSlack(text) {
   try {
-    const result = await slackClient.chat.postMessage({
+    const result = await qnaClient.chat.postMessage({
       channel: process.env.SLACK_QNA_CHANNEL,
       text: text,
     });
@@ -172,51 +174,8 @@ app.post('/qna', (req, res) => {
   const text = req.body.text;
   sendQnaMessageToSlack(text);
 });
-app.get('/qna', (req, res) => {
-  const slackToken = process.env.SLACK_QNA_TOKEN;
-  const client = new WebClient(slackToken);
-  // 대화 내역을 가져오기 위해 API 요청을 만듭니다.
-  const channel = process.env.SLACK_QNA_CHANNEL;
 
-  async function getUserName(userId) {
-    try {
-      const userInfo = await client.users.info({ user: userId });
-      return userInfo.user.real_name;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  async function getConversationHistory() {
-    try {
-      const response = await client.conversations.history({ channel });
-      const messages = response.messages;
-      // 각 메시지의 사용자 이름과 메시지 텍스트를 가져와 출력
-      let data = [];
-      for (const message of messages) {
-        let name = await getUserName(message.user);
-        name = name == 'Demo App' ? '' : name;
-        // 작성 날짜 표시
-        const date = new Date(message.ts * 1000);
-        // 줄바꿈 문자를 기준으로 첫 번째 줄과 나머지 줄 분리
-        const lines = message.text.split('\n');
-        const title = lines[0];
-        const content = lines.slice(1);
-        data.push({ name, date, title, content });
-      }
-      return data;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  getConversationHistory()
-    .then(response => {
-      console.log(response);
-      res.status(200).json(response);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
+
 app.get('/header', (req, res) =>
   res.status(200).json({ header: process.env.HEADER })
 );
